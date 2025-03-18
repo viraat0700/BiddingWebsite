d03603b84f6cf8e0d537ac0fbec45b1291293566
Yatharth-14 <yatharthyadav123@gmail.com>
2025-03-19 00:29:05 +0530

    building navbar

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Landing = () => {
   return (
     <div>
-      This is landing page
+      <nav className='border flex justify-between'>
+        <div className='flex m-2 px-2 gap-3 items-center'>
+            <b>BiddingWebsite</b>
+            <input className='border rounded p-1 focus:outline-none ' type="search" name="" id="search" title='searchbar' placeholder='Search your location' />
+        </div>
+        <div className='flex items-center m-2 px-2'>
+            <button className='border rounded-md p-1 px-2'>Login/SignUp</button>
+        </div>
+      </nav>
     </div>
   )
 }
