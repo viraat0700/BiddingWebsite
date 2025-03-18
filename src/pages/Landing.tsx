2f614bde49667f1262263254c8de4d823d836749
Yatharth-14 <yatharthyadav123@gmail.com>
2025-03-19 00:04:42 +0530

    fixed codebase

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Landing = () => {
   return (
     <div>
-      Landing page
+      This is landing page
     </div>
   )
 }
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,14 @@
+import Header from "../Components/Header"
+import MainContent from "../Components/MainContent"
+import Footer from "../Components/Footer"
+const Profile = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+
+export default Profile
